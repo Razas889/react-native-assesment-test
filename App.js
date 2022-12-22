715e95562ab8b5588f32e75ecc3d5d744bf896ee
Muhammad Shoaib Raza <mraza@ilsaintaractive.com>
2022-12-22 14:52:29 +0500

    removes extra imports.

--- App.js
+++ App.js
@@ -10,11 +10,6 @@ import {
 } from 'react-native';
 import Constants from 'expo-constants';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
 
 export default function App() {
 
